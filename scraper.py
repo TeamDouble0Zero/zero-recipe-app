776334736a7c6d204be10fe19939b51dd34c96f1
OnyxGloom <143751055+OnyxGloom@users.noreply.github.com>
2024-06-02 19:37:33 +0100

    KAN-10

--- scraper.py
+++ scraper.py
@@ -6,6 +6,8 @@ from bs4 import BeautifulSoup
 
 url = 'https://www.bbcgoodfood.com/recipes/oven-baked-risotto'
 
+#input("Choose a recipe url: ")
+
 recipeUrl = f"https://cooked.wiki/{url}"
 
 s = requests.session() 
@@ -15,8 +17,15 @@ response = response.content
 
 soup = BeautifulSoup(response, 'html.parser')
 
-ingredients = soup.findAll(name='ingredients')
+ingredientList = soup.find('div', class_='shopping-list')
 
-print(ingredients.value)
+ingredients = ingredientList.findAll('li', class_='ingredient')
+
+for item in ingredients:
+    box = item.find('label')
+    ingredient = box.find('input')
+    ing = ingredient.attrs['value']
+
+    print(ing)
 
 
