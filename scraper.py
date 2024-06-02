5535e2a3ff9d2049b295b2ac49de3e1727e8a37e
OnyxGloom <143751055+OnyxGloom@users.noreply.github.com>
2024-06-02 20:33:52 +0100

    KAN-10

--- scraper.py
+++ scraper.py
@@ -3,29 +3,55 @@ import urllib.request
 
 from bs4 import BeautifulSoup
 
-
-url = 'https://www.bbcgoodfood.com/recipes/oven-baked-risotto'
-
-#input("Choose a recipe url: ")
-
+#Asks user for URL for recipe and converts it
+url = input("Choose a recipe url: ")
 recipeUrl = f"https://cooked.wiki/{url}"
 
+#This stage allows the conversion redirect to happen
 s = requests.session() 
 response = s.get(recipeUrl) 
-
 response = response.content
 
+#Retrieves the HTML
 soup = BeautifulSoup(response, 'html.parser')
 
-ingredientList = soup.find('div', class_='shopping-list')
 
-ingredients = ingredientList.findAll('li', class_='ingredient')
+#This finds the ingredient list and prints to the terminal
+def retrieveIngredients():
+    ingredientList = soup.find('div', class_='shopping-list')
+    ingredients = ingredientList.findAll('li', class_='ingredient')
 
-for item in ingredients:
-    box = item.find('label')
-    ingredient = box.find('input')
-    ing = ingredient.attrs['value']
+    for item in ingredients:
+        box = item.find('label')
+        ingredient = box.find('input')
+        ing = ingredient.attrs['value']
 
-    print(ing)
+        print(ing)
+
+#Finds and prints recipe title
+def retrieveTitle ():
+    titleBox = soup.find('div', class_='title')
+    title = titleBox.text
+
+    print(title)
 
 
+#Finds the instructions, adds a step number, then prints result
+def retrieveInstructions():
+    lineNumber = 0
+    instructionPage = soup.find('div', class_='recipe-content')
+    instructionBox = instructionPage.find('ol')
+    instructions = instructionBox.findAll('li')
+
+    for item in instructions:
+        lines = item.find('p')
+        instruction = lines.text
+        lineNumber=lineNumber+1
+        print(str(lineNumber)+'.'+instruction)
+
+
+
+
+retrieveTitle ()
+retrieveIngredients()
+retrieveInstructions()
